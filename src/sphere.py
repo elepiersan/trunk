704a712bda9d96918b0fbe517be7d6ecacd995c9
elepiersan <piersanti.eleonora@gmail.com>
2017-07-24 11:44:51 -0700

    added a word file describing the model

--- src/sphere.py
+++ src/sphere.py
@@ -1,27 +1,27 @@
 from yade import pack
 import gts, os.path, locale
+from yade import qt
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')   #gts is locale-dependend.  If, for example, german locale is used, gts.read()-function does not import floats normally
 
 
-surf=gts.read(open('sphere.gts'))
+surf=gts.read(open('../mesh/sphere.gts'))
 
-if surf.is_closed():
-	pred=pack.inGtsSurface(surf)
-	aabb=pred.aabb()
-	dim0=aabb[1][0]-aabb[0][0]; radius=dim0/40. # get some characteristic dimension, use it for radius
-	O.bodies.append(pack.regularHexa(pred,radius=radius,gap=radius/4.))
-	#surf.translate(0,0,-(aabb[1][2]-aabb[0][2])) # move surface down so that facets are underneath the falling spheres
+idTissue=O.materials.append(FrictMat(young=500.0,poisson=.35,frictionAngle=.6,label="concrete"))
+pred=pack.inGtsSurface(surf)
+aabb=pred.aabb()
+
+dim0=aabb[1][0]-aabb[0][0];
+# brain radius
+dim0 = dim0/2.0
+# small
+radius=dim0/20. # get some characteristic dimension, use it for radius
+O.bodies.append(pack.regularHexa(pred, radius=radius, gap=0.0, material=idTissue))
+
 O.bodies.append(pack.gtsSurface2Facets(surf,wire=True))
 
-for b in O.bodies:
-	if b.shape.name!=Sphere: continue
-	if b.state.pos[2]<.5: 
-		b.dynamic=False
-		print b.state.pos
-    
 O.engines=[
-	#ForceResetter(),
+	ForceResetter(),
 	InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Facet_Aabb()],label='collider'),
 	InteractionLoop(
 		[Ig2_Sphere_Sphere_ScGeom(),Ig2_Facet_Sphere_ScGeom()],
@@ -30,17 +30,11 @@ O.engines=[
 	),
 	NewtonIntegrator(damping=.1,gravity=[0,0,0]),
 	PyRunner(iterPeriod=1000,command='timing.stats(); O.pause();'),
-	PyRunner(iterPeriod=10,command='addPlotData()')
 ]
 O.dt=.7*PWaveTimeStep()
 O.saveTmp()
 O.timingEnabled=True
 O.trackEnergy=True
-from yade import plot
-plot.plots={'i':('total',O.energy.keys,)}
-def addPlotData(): plot.addData(i=O.iter,total=O.energy.total(),**O.energy)
-plot.plot(subPlots=False)
 
-from yade import timing
-from yade import qt
-qt.View()
+a = qt.View()
+yade.qt._GLViewer.GLViewer.saveSnapshot(qt.View(), "sphere.png")
